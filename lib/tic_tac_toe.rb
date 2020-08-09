565825d3ed1bd169643eff819b38b0dce67399a3
Nathan Steed <steedn@outlook.com>
2020-08-09 19:43:28 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,5 +54,10 @@ class TicTacToe
     display_board
   end
   def won?
+    WIN_COMBINATIONS.any? do |combo|
+      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+        return combo
+      end
+    end
   end
 end
