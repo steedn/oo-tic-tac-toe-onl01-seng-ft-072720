be93f1094bd880726104cdf8f3fad8aa92f8b82e
Nathan Steed <steedn@outlook.com>
2020-08-09 19:06:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -41,7 +41,7 @@ class TicTacToe
   def turn
     puts "Please enter a number between (1-9)"
     index = gets
-    # input_to_index(index)
+    input_to_index(index)
     # valid_move(index)
     # current_player
 
