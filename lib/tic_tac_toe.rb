ecdd60955f92db708b0b07aac8137a5f902391c0
Nathan Steed <steedn@outlook.com>
2020-08-09 19:57:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,6 +3,7 @@ require "pry"
 class TicTacToe
   WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
+  attr_accessor :board
   def initialize
     @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   end
